63ce8e869bc6521c0b1cf72fe12e869f5de01a2f
marevik <igopko1996@gmail.com>
2018-04-07 22:29:26 +0300

    com
    1

---
+++ js/my.js
@@ -0,0 +1,512 @@
+'use strict'
+//var x = 10;
+
+function name() {
+    // var a = 6;
+    // var b = 11;
+    // var c = 21;
+    // var d = 4;
+
+//    if (a == 5) {
+//        for (var i = 1; i <= 10; i++) console.log(i);
+//    } else {
+//        console.log('a != 5');
+//    };
+//
+//    while (b <= 20) {
+//        console.log(b);
+//        b++;
+//    };
+//
+//    do {
+//        console.log(c);
+//        c++;
+//    } while (c <= 30);
+//
+//    switch (d) {
+//        case 4:
+//            console.log('Четвер');
+//            break;
+//        case 5:
+//            console.log('П"ятниця');
+//            break;
+//        default:
+//            console.log('No day');
+//    };
+//
+// };
+
+
+//
+// var a = 1;
+//
+// function name2 (){
+//     setInterval(function(){
+//         console.log(a);
+//         a++;
+//     },2000);
+//     console.log('My - ' + a);
+// }
+//
+// name2();
+//     var a1 = 9;
+//
+//     if (a1 < 10) {
+//         for (var i = 1; i<= 10; i++) console.log(i);
+//     } else {
+//         console.log("a1!<10")
+//     };
+//
+//     var a2=3;
+//     if (a2<=3){
+//         console.log("менше рівне 3")
+//     }else {
+//         console.log("більше 3")
+//     };
+//
+// var a3=22;
+// if(a3<10){
+//     console.log("malush")
+// }else if(a3>10, a3<20){
+//     console.log("normas")
+//     }else if(a3>20){
+//     console.log("da ty starik)")
+//     }
+// };
+//
+// var a4="lion";
+// if (a4="lion"){
+//     console.log("animal king")
+// }else {
+//     console.log("run animal, run")
+// };
+//
+//
+// var a5=5;
+// if(a5=="5"){
+//     console.log("number")
+// } else{
+//     console.log("eror")
+// };
+//
+// var a6='vse propalo';
+// if (a6="vse propalo"){
+//     console.log('norm')
+// }else{
+//     console.log("vse propalo")
+// };
+//
+// var a7='i don"t have idea';
+// if (a7==    "i don't have idea"){
+//     console.log(a7)
+// }else{
+//     console.log("eto fiasco")
+// };
+//
+// var a8;
+// if("mon"<"tue"){
+//     console.log("nice")
+// } else{
+//     console.log("so so")
+// }
+//
+// var a9=[1,3,4,5,6,78];
+// if (a9=[1,3,4,5,6,78]){
+//       console.log(a9);
+// } else {
+//     console.log('another condition')
+// };
+
+    // var b1 = 2;
+    // while (b1 <= 50) {
+    //     console.log(b1);
+    //     b1++;
+    // }
+    // ;
+    //
+    // var b2 = 3;
+    // while (b2 < "7") {
+    //     console.log(b2);
+    //     b2++;
+    // }
+    // ;
+    //
+    // var b3 = "mon";
+    // var b3_1 = "fri";
+    // while (b3 >= b3_1) {
+    //     console.log(true)
+    //     b3++;
+    // };
+    //
+    // var b4="lion";
+    // while (b4 =="animal"){
+    //     console.log(false)
+    //     b4++;
+    // };
+    //
+    // var b5=2+3;
+    // while (b5<30){
+    //     console.log("b5");
+    //     b5++;
+    // };
+    //
+    // var b6=3+3;
+    // while(b6<21 ){
+    //     console.log(b6);
+    //     b6++;
+    // };
+    //
+    // var b7=0;
+    // var b7_1=0;
+    // while (b7<12){
+    //     console.log(b7_1,b7);
+    //     b7++;
+    //     b7_1+=b7;
+    // };
+    //
+    // var b8=0;
+    // var b8_1=0;
+    // var b8_2=0;
+    // while (b8<10){
+    //     console.log(b8,b8_1,b8_2);
+    //     b8++;
+    //     b8_1+=b8;
+    //     b8_2=b8+b8_1+b8_2;
+    // };
+
+    // var b9=[1,32];
+    //
+    // while(b9>[0,1]){
+    //     console.log(b9[1]);
+    //     b9++;
+    // };
+    //
+
+
+// do {
+//        console.log(c);
+//        c++;
+//    } while (c <= 30);
+
+// var c1=2;
+// do{
+//     console.log(c1);
+//     c1++;
+// } while(c1<=6);
+//
+// var c2=2;
+// do{
+//     console.log("c2");
+//     c2++;
+// } while(c2<=6);
+//
+// var c3=2;
+// do{
+//     console.log(c3);
+//     c3++;
+// } while(c3<=10);
+//
+// var c4=[1,2];
+// do{
+//     console.log(c4[1]);
+//     c4++;
+// } while (c4<[9,11]);
+//
+// var c5=19+32;
+// do{
+//     console.log(c5);
+//     c5++;
+// } while (c5<55);
+//
+// var c6="a";
+// do{
+//     console.log(c6);
+//     c6++;
+// } while (c6<"Z");
+//
+// var c7=0
+//  var c7_1=0;
+//     do{
+//         console.log(c7, c7_1);
+//         c7++;
+//         c7_1+=c7;
+//     }while (c7<20);
+//
+//     var c8=0
+//     var c8_1=0;
+//     var c8_2=0;
+//     var c8_2=0;
+//     do{
+//         console.log(c8, c8_1, c8_2);
+//         c8++;
+//         c8_1+=c8;
+//         c8_2+=c8+c8_1;
+//     }while (c8<20);
+//
+//     var c9=0
+//     var c9_1=0;
+//     do{
+//         console.log(c9, c9_1);
+//         c9++;
+//         c9_1*=c9;
+//     }while (c9<20);
+
+
+// switch (d) {
+//        case 4:
+//            console.log('Четвер');
+//            break;
+//        case 5:
+//            console.log('П"ятниця');
+//            break;
+//        default:
+//            console.log('No day');
+//    };
+// var  d1=3;
+// switch(d1) {
+//     case 1:
+//         console.log('1');
+//         break;
+//         case 2:
+//         console.log('2');
+//         break;
+//         case 3:
+//         console.log('3');
+//         break;
+//         case 4:
+//         console.log('4');
+//         break;
+//         case 5:
+//         console.log('5');
+//         break;
+//         default:
+//             console.log("mdaa")
+//     };
+//
+//     var  d2=333;
+//     switch(d2) {
+//     case 1:
+//         console.log('1');
+//         break;
+//         case 2:
+//         console.log('2');
+//         break;
+//         case 3:
+//         console.log('3');
+//         break;
+//         case 4:
+//         console.log('4');
+//         break;
+//         case 5:
+//         console.log('5');
+//         break;
+//         default:
+//             console.log("mdaa")
+//     };
+//
+//     var  d3=6;
+//     switch(d3) {
+//     case 1:
+//         console.log('mon');
+//         break;
+//         case 2:
+//         console.log('tue');
+//         break;
+//         case 3:
+//         console.log('wed');
+//         break;
+//         case 4:
+//         console.log('thu');
+//         break;
+//         case 5:
+//         console.log('fri');
+//         break;
+//         default:
+//             console.log("else")
+//     };
+//
+//      var  d4=2;
+//     switch(d4) {
+//     case 1:
+//         console.log('lion');
+//         break;
+//         case 2:
+//         console.log('elephont');
+//         break;
+//         case 3:
+//         console.log('bear');
+//         break;
+//         case 4:
+//         console.log('beef');
+//         break;
+//         case 5:
+//         console.log('moncey');
+//         break;
+//         default:
+//             console.log("another animal")
+//     };
+//
+//     var  d5=5;
+//     switch(d5) {
+//     case 1:
+//         console.log('city');
+//         break;
+//         case 2:
+//         console.log('village');
+//         break;
+//         case 3:
+//         console.log('town');
+//         break;
+//         case 4:
+//         console.log('island');
+//         break;
+//         case 5:
+//         console.log('continent');
+//         break;
+//         default:
+//             console.log("another place")
+//     };
+//
+//     var  d5=3;
+//     switch(d5) {
+//     case 1:
+//         console.log('dead');
+//         break;
+//         case 2:
+//         console.log('live');
+//         break;
+//         default:
+//             console.log("another state")
+//     };
+//
+//     var  d6=1;
+//     switch(d6) {
+//     case 1:
+//         console.log('muse');
+//         break;
+//         case 2:
+//         console.log('RHCP');
+//         break;
+//         case 2:
+//         console.log('AC/DC');
+//         break;
+//         default:
+//             console.log("another group")
+//     };
+//
+//     var  d7=1;
+//     switch(d7) {
+//     case 1:
+//         console.log('lol');
+//         break;
+//         case 2:
+//         console.log('kek');
+//         break;
+//         case 2:
+//         console.log('chebureck');
+//         break;
+//         default:
+//             console.log("another word")
+//     };
+//
+//     var  d8=2;
+//     switch(d8) {
+//     case 1:
+//         console.log('i don"t know');
+//         break;
+//         case 2:
+//         console.log('i can"t do');
+//         break;
+//         case 2:
+//         console.log('i don"t want');
+//         break;
+//         default:
+//             console.log("another smear")
+//     };
+//
+//     var  d9=1;
+//     switch(d9) {
+//     case 1:
+//         console.log('i  know');
+//         break;
+//         case 2:
+//         console.log('i  do');
+//         break;
+//         case 2:
+//         console.log('i  want');
+//         break;
+//         default:
+//             console.log("another oportunity")
+//     };
+
+// if (a == 5) {
+//        for (var i = 1; i <= 10; i++) console.log(i);
+//    } else {
+//        console.log('a != 5');
+//    };
+// var e1=0;
+// for (; e1 < 9; e1++) {
+//     console.log(e1);
+// };
+//
+// var e2_1=1;
+// for (var e2 = 0; e2 < 9; e2++) {
+//    e2_1 += e2;
+//    console.log(e2_1)
+// };
+//
+// var e3_1=0;
+// var e3_2=0;
+// for( var e3=0; e3<10; e3++){
+//     console.log(e3, e3_1, e3_2);
+//     e3_1+=e3;
+//     e3_2+=e3_1+e3;
+// };
+//
+// var e4_1=1;
+// for (var e4 = 0; e4 < 7; e4++) {
+//    e4_1 += e4;
+//    console.log("e4_1", "e4");
+// };
+//
+// var e5_1=1;
+// for (var e5 = 0; e5 < 7; e5++) {
+//    e5_1 += e5;
+//    console.log("e5_1"+e5);
+// };
+//
+// var e6_1=1;
+// for (var e6 = 0; e6 < 15; e6++) {
+//    e6_1 += e6;
+//    console.log(e6_1+e6);
+// };
+//
+// var e7_1=1;
+// for (var e7 = 0; e7 < 7; ++e7) {
+//    e7_1 += e7/2;
+//    console.log(e7_1*e7);
+// };
+
+var e8_1=[1,3,56,7,2];
+for(var e8=0; e8<e8_1.length; e8++){
+    e8_1[e8]*=2;
+    console.log(e8_1, e8);
+};
+
+var e9={
+    p1:1,
+    p2:2
+};
+for(var p in e9){
+    console.log(p);
+    e9[p]=e9[p]+1;
+};
+}
+name();
+
+
+
+
+
+
+
